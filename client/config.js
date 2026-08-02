1b0b6bc3e6e65ef8d40b78dfb713665950755ff0
b3nzy <bantzviper21st@gmail.com>
2026-08-02 18:10:18 +0200

    Point client at live Render API

--- client/config.js
+++ client/config.js
@@ -1,3 +1,3 @@
 // Change this to your deployed API URL when you deploy (see the deployment guide).
 // Local development default assumes the server from /server is running on port 4000.
-window.API_BASE = 'http://localhost:4000/api';
+window.API_BASE = 'https://orbit-planner-xd8z.onrender.com/api';
